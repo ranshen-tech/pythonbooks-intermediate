9fab380fedb5e303d674c775860d55e93232bedb
冉申 <ranshen0519@icloud.com>
2021-11-18 14:23:22 +0800

    Create exercise_5.py
    Co-Authored-By: 冉申 <75344071+ranshen-tech@users.noreply.github.com>

---
+++ exercise_5.py
@@ -0,0 +1,24 @@
+name ='Zed A. Shaw'
+age =35# not a lie
+height =74# inches
+weight =180# lbs
+eyes ='Blue'
+teeth ='White'
+hair ='Brown'
+
+height =height*2.54# cm
+weight =weight*0.454# kg
+
+print(f"Let's talk about {name}.")
+print(f"He's {height}cm tall.")
+print(f"He's {weight}kg heavy.")
+print("Actually that's not too heavy.")
+print(f"He's got {eyes} eyes and {hair} hair.")
+print(f"His teeth are usually {teeth} depending on the coffee.")
+
+# this line is tricky, try to get it exactly right
+total  =age +height +weight
+print(f"If I add {age}, {height}, and {weight} I get {total}.")
+
+msg =round(1.7333, 2)
+print(msg)
