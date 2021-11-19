b948539bafeb183667c9f7e9d0639d1ca989c341
冉申 <ranshen0519@icloud.com>
2021-11-19 18:36:05 +0800

    Create exercise_9.py

---
+++ exercise_9/exercise_9.py
@@ -0,0 +1,16 @@
+# Here's some new strange stuff, remember type it exactly.
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months= "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+# days被赋值什么，就打印什么，Ture,False,None都如实打印
+print("Here are the days: ", days)
+print("Here are the months: ", months)
+
+# 这里肯定发生了什么事；用三个双引号，我们可以随心所欲地打字，甚至4行，5行，6行
+# 记住这个格式
+print("""
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+""")
