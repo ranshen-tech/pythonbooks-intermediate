6da2d97fe9dacf424ee9c25cd2a1ccc2672b545a
冉申 <ranshen0519@icloud.com>
2021-11-26 18:01:41 +0800

    Create consolidation_exercise.py
    Draft
    
    Co-Authored-By: 冉申 <ranshen0519@icloud.com>

---
+++ consolidation_exercise.py
@@ -0,0 +1,76 @@
+# from sys import argv
+# script, filename = argv
+
+# txt = open(filename)
+
+# print("Here is your file:", filename)
+# print(txt.read())
+
+
+# from sys import argv
+
+# script, filename = argv
+# txt = open(filename)
+
+# print(f"Here is your file:", {filename})
+# print(txt.read())
+
+
+# list = ['a', 'b', 'A', 'C', 'd', 'c']
+# print(list)
+# print(sorted(list))
+# print(list)
+
+# spam = 67
+
+# if spam == 1:
+#     print('Hello')
+# elif spam == 2:
+#     print('Howdy')
+# else:
+#     print('Greeting')
+
+# for i in range(1, 11):
+#     print(i)
+# print()
+
+# a = 1
+# while a <= 10:
+#     print(a)
+#     a += 1
+
+# round()函数返回浮点数x四舍五入的值
+# number = 3.1415926
+# print(round(number, 2))
+# print(number)
+# a = round(number, 3)
+# print(a)
+# print(number)
+# round(number, 3)
+# print(number)
+# print(round(number, 0))
+
+# print(14//8)
+
+# 💖💖好题，值得细品
+# i = sum = 0
+
+# while i <= 4:
+#     sum += i
+#     i = i+1
+# print(sum)
+# print(i)
+
+# abs函数返回绝对值
+# a = 4.3
+# print(abs(4.3))
+# print(abs(a))
+# abs(a)
+
+x = [1, 2, 3, 4, 5, 6]
+x[1] = 'two'
+print(x)
+
+
+x[2:5] = [3.0, 4.0, 5.0]
+print(x)
